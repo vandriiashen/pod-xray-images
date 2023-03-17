Corrected confidence interval computation

analyze_res.py
@@ -1,8 +1,9 @@
 import numpy as np
 from pathlib import Path
 import statsmodels.api as sm
+from sklearn.preprocessing import PowerTransformer
 import matplotlib.pyplot as plt
-from matplotlib import ticker, colors
+from matplotlib import ticker, colors  
 
 def compute_s90(fit, x_range=np.linspace(0., 3., 1000)):
     '''Computes s_90 - defect size for 90\% probability of good segmentation and s_90/95 - lower bound of the same value for 95% confidence interval.
@@ -16,10 +17,14 @@ def compute_s90(fit, x_range=np.linspace(0., 3., 1000)):
     :return: List containing s_90 and s_90/95. If any does not exist, it is replaced with -1.
     :rtype: :class:`list`
     '''
-    nu = fit.params[0]*x_range + fit.params[1]
-    nu_low = (fit.params[0]-2*fit.bse[0])*x_range + (fit.params[1]-2*fit.bse[1])
-    p = 1 / (1+np.exp(-nu))
-    p_low = 1 / (1+np.exp(-nu_low))
+    fit_x = np.ones((x_range.shape[0], 2))
+    fit_x[:,0] = x_range
+    prediction = fit.get_prediction(fit_x)
+    fit_y = prediction.summary_frame(alpha=0.05)
+    
+    p = fit_y['mean']
+    p_low = fit_y['mean_ci_lower']
+    p_high = fit_y['mean_ci_upper']
     
     res = [-1., -1.]
     s90_95_exists = np.any(p_low > 0.9)
@@ -32,24 +37,30 @@ def compute_s90(fit, x_range=np.linspace(0., 3., 1000)):
         res[1] = s90_95
     return res
 
-def stat_analyze(res):
+def stat_analyze(res, y_val='Mean'):
     '''Performs binomial fit on the model results to determine POD properties.
     Segmentation accuracy is transformed into a binary outcome. A segmentation is considered good if F1 score is higher than 50%.
     
     :param res: Array with model results. Should contain FO thickness and average accuracy of segmentation
     :type res: :class:`np.ndarray`
+    :param y_val: Field of the results to use as model F1. By default, use mean value, but individual instances can also be used.
+    :type y_val: :class:`str`
     
     :return: Statsmodels object with fit results
     :rtype: :class:`statsmodels.genmod.generalized_linear_model.GLMResultsWrapper`
     '''    
-    y = res['Mean'] > 0.5
-    x = np.zeros((res['FO_th'].shape[0], 2))
+    y = res[y_val] > 0.5
+    x = np.ones((res['FO_th'].shape[0], 2))
     x[:,0] = res['FO_th']
-    x[:,1] = 1
 
     glm = sm.GLM(y, x, family=sm.families.Binomial(link=sm.families.links.Logit()))
+    #glm = sm.GLM(y, x, family=sm.families.Binomial(link=sm.families.links.CDFLink()))
+    #glm = sm.GLM(y, x, family=sm.families.Binomial(link=sm.families.links.probit()))
+    #glm = sm.GLM(y, x, family=sm.families.Binomial(link=sm.families.links.LogLog()))
+    #glm = sm.GLM(y, x, family=sm.families.Binomial(link=sm.families.links.cauchy()))
     fit = glm.fit()
     #print(fit.summary())
+    #print('Akaike information criterion = ', fit.info_criteria(crit='aic'))
     return fit
 
 def draw_pod(ax, fit, res=None, default_x_range=np.linspace(0., 3., 1000), draw_confidence_interval=True, draw_s90=False, label='POD', colors = ['r', 'b']):
@@ -79,12 +90,14 @@ def draw_pod(ax, fit, res=None, default_x_range=np.linspace(0., 3., 1000), draw_
         x = res['FO_th']
         x_range = np.linspace(x.min(), x.max(), 1000)
     
-    nu = fit.params[0]*x_range + fit.params[1]
-    nu_low = (fit.params[0]-2*fit.bse[0])*x_range + (fit.params[1]-2*fit.bse[1])
-    nu_high = (fit.params[0]+2*fit.bse[0])*x_range + (fit.params[1]+2*fit.bse[1])
-    p = 1 / (1+np.exp(-nu))
-    p_low = 1 / (1+np.exp(-nu_low))
-    p_high = 1 / (1+np.exp(-nu_high))
+    fit_x = np.ones((x_range.shape[0], 2))
+    fit_x[:,0] = x_range
+    prediction = fit.get_prediction(fit_x)
+    fit_y = prediction.summary_frame(alpha=0.05)
+    
+    p = fit_y['mean']
+    p_low = fit_y['mean_ci_lower']
+    p_high = fit_y['mean_ci_upper']
     
     s90, s90_95 = compute_s90(fit, x_range=x_range)
     s90_exists = True if s90 > -1 else False
@@ -131,7 +144,7 @@ def draw_F1(ax, res):
     ax.yaxis.set_major_formatter(ticker.PercentFormatter(1.))
     ax.grid(True)
     
-def draw_segm_ratio(ax, res):
+def draw_segm_ratio(ax, res, alpha=1.):
     '''Draws an approximation of POD on ax
     F1 scores are transformed into binary outcomes by checking if F1 > 50%
     Then the range of defect sizes is binned, and in every bin a fraction of cases with F1 > 50% is computed
@@ -140,13 +153,15 @@ def draw_segm_ratio(ax, res):
     :type ax: :class:`matplotlib.axes._subplots.AxesSubplot`
     :param res: Array with model results
     :type res: :class:`np.ndarray`
+    :param alpha: Opacity of the bins
+    :type alpha: :class:`np.ndarray`
     '''
     y = res['Mean']
     x = res['FO_th']
 
     bins = np.linspace(x.min(), x.max(), 20)
     bin_width = bins[1]-bins[0]
-    print(bins)
+    #print(bins)
     prob = np.zeros_like(bins)
     for i in range(bins.shape[0]-1):
         mask = np.logical_and(x >= bins[i], x < bins[i+1])
@@ -157,7 +172,7 @@ def draw_segm_ratio(ax, res):
         else:
             prob[i] = 1
     prob[-1] = prob[-2]
-    ax.bar(bins, prob, bin_width, align='edge')
+    ax.bar(bins, prob, bin_width, align='edge', alpha=alpha)
     ax.set_xlabel('Defect size, mm', fontsize=16)
     ax.set_ylabel("Fraction of cases with F1 > 50%", fontsize=16)
     ax.set_ylim(0., 1.)
@@ -185,12 +200,14 @@ def show_pod_pipeline(res, fit):
     draw_segm_ratio(ax2, res)
     ax2.set_title('(b)', y = -0.19, fontsize=16, weight='bold')
     
+    draw_segm_ratio(ax3, res, alpha = 0.2)
     draw_pod(ax3, fit, res=res, draw_s90=True, label='POD', colors = ['r', 'b'])
     ax3.legend(loc=2, fontsize=16)
     ax3.set_title('(c)', y = -0.15, fontsize=16, weight='bold')
     
     plt.tight_layout()
     plt.savefig('tmp_img/pod_pipeline.pdf', format='pdf')
+    plt.savefig('tmp_img/pod_pipeline.png')
     plt.show()
     
 def comp_models(fits, fit_labels, draw_s90 = False, x_range = np.linspace(0., 3., 1000)):
@@ -295,8 +312,13 @@ def load_res_file(fname):
     '''
     res = np.genfromtxt(fname, delimiter=',', names=True)
     raw_total = res.shape[0]
-    mask = res['BO_fract'] > 0.9
-    res = res[mask]
+    #scat_thr = res['Scat_fract'].max()
+    #mask = res['Scat_fract'] > 0.2 * scat_thr
+    #mask = res['BO_fract'] > 0.8
+    #mask = res['Cyl_r'] > 15.
+    #mask = np.logical_and(res['Cyl_r'] > 0., res['Cyl_r'] < 5.)
+    #res = res[mask]
+    #res = res[:100]
     filtered_total = res.shape[0]
     print("After filter: {}/{}".format(filtered_total, raw_total))
     return res
@@ -304,6 +326,8 @@ def load_res_file(fname):
 def single_test(model_name):
     res = load_res_file('./test_res/{}.csv'.format(model_name))
     fit = stat_analyze(res)
+    s90, s90_95 = compute_s90(fit)
+    print('s_90 = {:.3f} | s_90/95 = {:.3f}'.format(s90, s90_95))
     show_pod_pipeline(res, fit)
 
 def batch_test(mat_name = 'pl90', draw_s90=False):
@@ -339,10 +363,47 @@ def heatmap_comparison(mat_name = 'pl90'):
     res_mc_mc = load_res_file('./test_res/{}_mc_mc.csv'.format(mat_name))
     
     compare_bo_fo(res_r_mc, res_mc_mc)
+    
+def compare_instances(model_name):
+    res = load_res_file('./test_res/{}.csv'.format(model_name))
+    
+    fig, ax = plt.subplots(1, 1, figsize=(12,9))
+    
+    iterations = sorted(filter(lambda x: x.startswith('Iter'), res.dtype.names))
+    iterations.append('Mean')
+    p1_arr = np.zeros((len(iterations)))
+    p2_arr = np.zeros((len(iterations)))
+    s90s_arr = np.zeros((len(iterations)))
+    s9095s_arr = np.zeros((len(iterations)))
+    for i, it in enumerate(iterations):
+        fit = stat_analyze(res, it)
+        a, b = fit.params
+        s90, s90_95 = compute_s90(fit)
+        p1_arr[i] = a
+        p2_arr[i] = b
+        s90s_arr[i] = s90
+        s9095s_arr[i] = s90_95
+        if it == 'Mean':
+            draw_confidence_interval=True
+            draw_pod(ax, fit, default_x_range=np.linspace(0., 3., 1000), draw_confidence_interval=draw_confidence_interval, draw_s90=False, label=it, colors = ['g', 'b'])
+        else:
+            draw_confidence_interval=False
+            draw_pod(ax, fit, default_x_range=np.linspace(0., 3., 1000), draw_confidence_interval=draw_confidence_interval, draw_s90=False, label=it, colors = ['r', 'b'])
+        
+    print('a        = {:.2f} +- {:.3f}'.format(p1_arr.mean(), p1_arr.std()))
+    print('b        = {:.2f} +- {:.3f}'.format(p2_arr.mean(), p2_arr.std()))
+    print('s90      = {:.2f} +- {:.3f}'.format(s90s_arr.mean(), s90s_arr.std()))
+    print('s90_95%  = {:.2f} +- {:.3f}'.format(s9095s_arr.mean(), s9095s_arr.std()))
+    
+    plt.tight_layout()
+    plt.savefig('tmp_img/comp_inst.pdf', format='pdf')
+    plt.savefig('tmp_img/comp_inst.png')
+    plt.show()
 
 if __name__ == "__main__":
     #materials are pl90, fe450 (+biron, +fe450biron), fe300
-    batch_test('fe300', draw_s90=False)
-    #single_test('fe450biron')
+    batch_test('biron', draw_s90=False)
+    #single_test('fe450_mc_mc')
     #mat_comparison('biron', 'fe450')
     #heatmap_comparison('fe450')
+    #compare_instances('fe450_mc_mc')

apply.py
@@ -32,13 +32,12 @@ def compute_f1(pred, tg):
     return f1
 
 def extract_fo_properties(mat):
-    '''Computes different image properties of the defect based on the distribution of material thickness
-    UPDATE!
+    '''Computes different properties of the defect based on the distribution of material thickness
     
     :param mat: 2D material thickness distribution (line integrals of segmentation). Consists of 2 channels: cylinder material and void (defect)
     :type mat: :class:`list`
     
-    :return: List of properties: the largest thickness of the defect, its area and thickness of the main object in location of the defect
+    :return: List of properties: location of the defect (array mask), the largest thickness and area
     :rtype: :class:`tuple`
     '''
     mat = np.array(mat)
@@ -47,16 +46,34 @@ def extract_fo_properties(mat):
     
     area = np.count_nonzero(fo)
     if area > 0:
-        min_th = mat[0,fo_map > 0].min()
         fo_th = fo_map.max()
-        bo_th = mat[0,:].ravel()[np.argmax(fo_map)]
-        bo_fract = bo_th / mat[0,:].max()
     else:
-        bo_th = 0
         fo_th = 0
-        bo_fract = 0
     
-    return fo_th, area, bo_th, bo_fract
+    return fo, fo_th, area
+
+def extract_im_properties(fo, log, proj, scat):
+    '''Computes different image properties of the projection and scattered signal
+    
+    :param fo: Defect mask
+    :type fo: :class:`np.ndarray`
+    :param log: Logarithm-corrected projection to extract attenuation intensity
+    :type log: :class:`np.ndarray`
+    :param proj: Raw projection before corrections
+    :type proj: :class:`np.ndarray`
+    :param scat: Distribution of scattered X-ray
+    :type scat: :class:`np.ndarray`
+
+    
+    :return: List of properties of the defect region: average attenuation and scattering to primary ratio
+    :rtype: :class:`tuple`
+    '''
+    mean_att = log[fo].mean()
+    total_primary = proj[fo].sum()
+    total_scattered = proj[fo].sum()
+    scat_fract = total_scattered / total_primary
+    
+    return mean_att, scat_fract
 
 def make_comparison(inp, tg, pred, fname):
     '''Visualizes the network prediction and compares it with ground-truth
@@ -158,20 +175,31 @@ def test_model(test_folder, base_name):
     :rtype: :class:`np.ndarray`
     '''
     save_folder = Path('./network_state/')
-    mat_fnames = sorted((test_folder / '../../mat').glob('*.tiff'))
+    inp_fnames = sorted((test_folder / 'inp').glob('*.tiff'))
+    stats_file = np.genfromtxt(test_folder / '../../stats.csv', delimiter=',', names=True)
     
     networks = [x for x in save_folder.iterdir() if x.is_dir()]
+    print(networks)
+    print(base_name)
     networks = sorted(filter(lambda x: x.name.startswith(base_name), networks))
+    print(networks)
     num_networks = len(networks)
     
     # First 3 fields are image properties
     image_fields = 4
-    res_arr = np.zeros((len(mat_fnames), image_fields+num_networks+1))
+    res_arr = np.zeros((len(inp_fnames), image_fields+num_networks+1))
     
-    for i in range(len(mat_fnames)):
-        mat = imageio.mimread(mat_fnames[i])
-        fo_th, area, bo_th, bo_fract = extract_fo_properties(mat)
-        res_arr[i,:image_fields] = fo_th, area, bo_th, bo_fract
+    for i in range(len(inp_fnames)):
+        im_num = int(inp_fnames[i].stem)
+        cyl_r = stats_file['cyl_r'][im_num]
+        cav_r = stats_file['cav_r'][im_num]
+        mat = imageio.mimread(test_folder / '../../mat' / inp_fnames[i].name)
+        log = imageio.imread(inp_fnames[i])
+        proj = imageio.imread(test_folder / '../../proj' / inp_fnames[i].name)
+        scat = imageio.imread(test_folder / '../../scat' / inp_fnames[i].name)
+        fo_mask, fo_th, area = extract_fo_properties(mat)
+        fo_att, scat_fract = extract_im_properties(fo_mask, log, proj, scat)
+        res_arr[i,:image_fields] = fo_th, area, fo_att, scat_fract
     
     for i in range(num_networks):
         nn_name = networks[i]
@@ -190,16 +218,16 @@ def test_model(test_folder, base_name):
 def batch_mode(test_root, res_folder, mat_name = 'pl90'):
     res_arr = test_model(test_root / '{}_test/radon/test'.format(mat_name), '{}_r'.format(mat_name))
     np.savetxt(res_folder / '{}_r_r.csv'.format(mat_name), res_arr, delimiter=',',
-                header='FO_th,Area,BO_th,BO_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
+                header='FO_th,Area,FO_att,Scat_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
     res_arr = test_model(test_root / '{}_test/mc/test'.format(mat_name), '{}_r'.format(mat_name))
     np.savetxt(res_folder / '{}_r_mc.csv'.format(mat_name), res_arr, delimiter=',',
-                header='FO_th,Area,BO_th,BO_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
+                header='FO_th,Area,FO_att,Scat_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
     res_arr = test_model(test_root / '{}_test/radon/test'.format(mat_name), '{}_mc'.format(mat_name))
     np.savetxt(res_folder / '{}_mc_r.csv'.format(mat_name), res_arr, delimiter=',',
-                header='FO_th,Area,BO_th,BO_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
+                header='FO_th,Area,FO_att,Scat_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
     res_arr = test_model(test_root / '{}_test/mc/test'.format(mat_name), '{}_mc'.format(mat_name))
     np.savetxt(res_folder / '{}_mc_mc.csv'.format(mat_name), res_arr, delimiter=',',
-                header='FO_th,Area,BO_th,BO_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
+                header='FO_th,Area,FO_att,Scat_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
     
 if __name__ == "__main__":
     test_root = Path('/export/scratch2/vladysla/Data/Simulated/MC/Server_tmp')
@@ -218,4 +246,4 @@ if __name__ == "__main__":
         test_folder = test_root / args.test
         res_arr = test_model(test_folder, args.name)
         np.savetxt(res_folder / '{}.csv'.format(args.out), res_arr, delimiter=',',
-                header='FO_th,Area,BO_th,BO_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))
+                header='FO_th,Area,FO_att,Scat_fract,Mean,' + ','.join(['Iter{}'.format(i+1) for i in range(res_arr.shape[1]-5)]))